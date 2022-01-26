28c03e522d649cba0ec142dfdeb3efa220d6531e
Acer <tnazarenok@gmail.com>
2022-01-26 19:38:46 +0300

    Initial commit + method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        
+        // write your code here
+
+    }
+        public static int add (int a, int b) {
+        return a+b;
     }
 }
